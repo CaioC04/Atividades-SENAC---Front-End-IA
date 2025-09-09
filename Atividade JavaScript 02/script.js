d7d71414a87c1c84ed34944c93a3ff9d7e276060
CAIO CARVALHO COSTA DE SOUZA <carvalho.caio@aluno.senac.rio>
2025-09-08 21:16:34 -0300

    Desafios JS

---
+++ Atividade JavaScript 02/script.js
@@ -0,0 +1,17 @@
+// nota = prompt("Digite a sua nota: ");
+// if (nota >= 7) {
+//     document.getElementById("nota").innerHTML = "Aprovado";
+// }
+// else if (nota < 5) {
+//     document.getElementById("nota").innerHTML = "Reprovado";
+// }
+// else if ( nota >= 5 && nota < 7) {
+//     document.getElementById("nota").innerHTML = "Recuperacao";
+// }
+
+function f1(){
+    for (i=1; i<11; i++){
+        document.getElementById("teste").innerHTML += " " + i;
+        console.log(i);
+    }
+}
